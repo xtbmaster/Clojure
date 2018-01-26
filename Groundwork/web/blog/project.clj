ffa68e136291edb689c256c84773a3652006f51c
arthur.aliev <int#nsive>
2018-01-26 16:59:52 +0200

    dummy content added

--- Groundwork/web/blog/project.clj
+++ Groundwork/web/blog/project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure        "1.9.0"]
-                  [ring/ring-core            "1.6.3"]
-                  [org.immutant/web          "2.1.10"]
-                  [compojure                 "1.6.0"]])
+  :dependencies [[org.clojure/clojure       "1.9.0"]
+                 [ring/ring-core            "1.6.3"]
+                 [org.immutant/web          "2.1.10"]
+                 [compojure                 "1.6.0"]
+                 [rum                       "0.10.8"]
+                 [org.clojure/clojurescript "1.9.946" :scope "provided"]])
--- Groundwork/web/blog/src/blog/server.clj
+++ Groundwork/web/blog/src/blog/server.clj
@@ -1,17 +1,46 @@
 (ns blog.server
   (:require
+   [rum.core :as rum]
    [immutant.web :as web]
    [compojure.core :as cj]
-   [compojure.route :as cjr]))
+   [compojure.route :as cjr]
+   ))
 
-(def app
-  (fn [req]
-    {:status 200
-     :body   (:uri req)}))
+(def posts
+  [{:id "123"
+     :created #inst "2018-01-26"
+     :author "aaliiev"
+     :body "As any other company, Wix started out small. One development team, one product, one code repository. As Wix started to grow with more development teams, GitHub seemed to be a right fit for us, as our version control service. Soon after, new problems started to arise and everything went out of order. To properly manage the chaos, new rules were formed, but it was not enough. Human factors errors remained to be an issue and the biggest source of security risks — this is where the story of Automation GitHub Services begins."}
+    {:id "456"
+      :created #inst "2018-01-25"
+      :author "arhur"
+      :body "The goal of this article is to provide a fairly comprehensive introduction to the Clojure programming language. A large number of features are covered, each in a fairly brief manner. Feel free to skip around to the sections of most interest. The section names in the table of contents are hyperlinks to make this easier when reading on-line.
+
+"}])
+
+(rum/defc post [post]
+  [:.post
+    [:.post_sidebar
+      [:img.avatar {:src (str "/i/" (:author post) ".jpg")}]]
+    [:div
+      [:p [:span.author (:author post) ": "] (:body post)]
+      [:p.meta (:created post) "//" [:a {:href (str "/post/" (:id post))} "Link"]]]])
+
+(rum/defc index [posts]
+  [:html
+    [:body
+      (for [p posts]
+        (post p))]])
 
 (cj/defroutes routes
   (cj/GET "/" [:as req]
-    { :body "INDEX"}))
+    {:body (rum/render-static-markup (index posts)) })
+  (cj/GET "/write" [:as req]
+    {:body "WRITE"})
+  (cj/POST "/write" [:as req]
+    {:body "POST"}))
+
+(def app routes)
 
 (defn -main [& args]
   (let [args-map (apply array-map args)
