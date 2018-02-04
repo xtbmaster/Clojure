68950c6ca75b59418293ab29ff2fd6740ebbbe56
xtbmaster <arturaliiev@gmail.com>
2018-02-04 23:09:11 +0200

    raw css added

--- Groundwork/web/blog/src/blog/server.clj
+++ Groundwork/web/blog/src/blog/server.clj
@@ -18,6 +18,77 @@
 
 "}])
 
+(def styles
+  "body {
+  font-family: Helvetica, Arial, sans-serif;
+  font-size: 1em;
+  line-heigh: 140%;
+  padding: 0.25em 1em;
+  max-width: 640px;
+  margin: 0 auto;
+  }
+
+  img {
+  max-width: 100%;
+  height:auto;
+  }
+
+  hr {
+  border: 0;
+  height: 0;
+  border-top: 1px solid rgba(0, 0, 0, 0.1);
+  border-bottom: 1px solid rgba(255, 255, 255, 0.3);
+  margin-bottom: 2em;
+  }
+
+  header {
+  margin-bottom: 2em;
+  }
+
+  header h1{
+  margin-bottom: 0;
+  }
+
+  header p{
+  margin-left: 0.09em;
+  }
+
+  .post{
+  display: flex;
+  justify-content: flex-start;
+  margin-bottom: 4em;
+  }
+
+  .post_sidebar {
+  text-align: center;
+  margin-right: 20px;
+  min-width: 50px;
+  }
+
+  .post_sidebar a:visited {
+  color: blue;
+  }
+
+  img.avatar {
+  border-radius: 100%;
+  width: 50px;
+  heigh: 50px;
+  }
+
+  .meta{
+  font-size: bold;
+  }
+
+  .author{
+  font-weight: bold;
+  }
+
+  footer{
+  padding-top: 1em;
+  margin-top: 1em;
+  border-top: 1px dotted black;
+  }")
+
 (rum/defc post [post]
   [:.post
     [:.post_sidebar
@@ -26,21 +97,43 @@
       [:p [:span.author (:author post) ": "] (:body post)]
       [:p.meta (:created post) "//" [:a {:href (str "/post/" (:id post))} "Link"]]]])
 
-(rum/defc index [posts]
+(rum/defc page [title & children]
   [:html
-    [:body
+    [:head
+      [:meta {:http-equiv "Content-type" :content "text/html; charset=UTF-8"}]
+      [:style styles]]
+      [:body
+        [:header
+          [:h1 "Стая"]
+            [:p#site_subtitle "This is a link."]]
+        children]])
+
+(rum/defc index [posts]
+  (page "Стая"
       (for [p posts]
-        (post p))]])
+        (post p))))
+
+(defn render-html [component]
+  (str "<!DOCTYPE html>\n" (rum/render-static-markup component)))
 
 (cj/defroutes routes
   (cj/GET "/" [:as req]
-    {:body (rum/render-static-markup (index posts)) })
+    {:body (render-html (index posts)) })
   (cj/GET "/write" [:as req]
     {:body "WRITE"})
   (cj/POST "/write" [:as req]
     {:body "POST"}))
 
-(def app routes)
+(defn with-headers [handler headers]
+  (fn [request]
+    (some-> (handler request)
+      (update :headers merge headers))))
+
+(def app
+  (-> routes
+    (with-headers { "Content-Type" "text/html; charset=utf-8"
+                    "Cache-control" "no-cache"
+                    "Expires" "-1"})))
 
 (defn -main [& args]
   (let [args-map (apply array-map args)
