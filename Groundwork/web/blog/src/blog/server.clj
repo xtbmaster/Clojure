3532de4de2257764ae8b9181b029046d4e009b1c
xtbmaster <arturaliiev@gmail.com>
2018-02-06 22:49:57 +0200

    resources added

--- Groundwork/web/blog/src/blog/server.clj
+++ Groundwork/web/blog/src/blog/server.clj
@@ -4,19 +4,22 @@
    [immutant.web :as web]
    [compojure.core :as cj]
    [compojure.route :as cjr]
-   ))
+   )
+  (:import
+    [org.joda.time DateTime]
+    [org.joda.time.format DateTimeFormat]))
 
 (def posts
   [{:id "123"
      :created #inst "2018-01-26"
-     :author "aaliiev"
-     :body "As any other company, Wix started out small. One development team, one product, one code repository. As Wix started to grow with more development teams, GitHub seemed to be a right fit for us, as our version control service. Soon after, new problems started to arise and everything went out of order. To properly manage the chaos, new rules were formed, but it was not enough. Human factors errors remained to be an issue and the biggest source of security risks — this is where the story of Automation GitHub Services begins."}
+     :author "arthur"
+     :body "As any other company, Wix started out small. One development team, one product, one code repository. As Wix started to grow with more development teams, GitHub seemed to be a right fit for us, as our version control service. Soon after, new problems started to arise and everything went out of order. To properly manage the chaos, new rules were formed, but it was not enough. Human factors errors remained to be an issue and the biggest source of security risks — this is where the story of Automation GitHub Services begins."
+     :pictures ["rab.gif"]}
     {:id "456"
       :created #inst "2018-01-25"
-      :author "arhur"
-      :body "The goal of this article is to provide a fairly comprehensive introduction to the Clojure programming language. A large number of features are covered, each in a fairly brief manner. Feel free to skip around to the sections of most interest. The section names in the table of contents are hyperlinks to make this easier when reading on-line.
-
-"}])
+      :author "arthur"
+      :body "The goal of this article is to provide a fairly comprehensive introduction to the Clojure programming language. A large number of features are covered, each in a fairly brief manner. Feel free to skip around to the sections of most interest. The section names in the table of contents are hyperlinks to make this easier when reading on-line."
+      :pictures ["rab.gif"]}])
 
 (def styles
   "body {
@@ -89,24 +92,55 @@
   border-top: 1px dotted black;
   }")
 
+(def date-formatter (DateTimeFormat/forPattern "dd.MM.YYYY"))
+
+(defn render-date [inst]
+  (.print date-formatter (DateTime. inst)))
+
 (rum/defc post [post]
   [:.post
     [:.post_sidebar
-      [:img.avatar {:src (str "/i/" (:author post) ".jpg")}]]
+      [:img.avatar {:src (str "/i/" (:author post) ".gif")}]]
     [:div
+      (for [name (:pictures post)]
+        [:img {:src (str "/post" (:id post) "/" name)}])
       [:p [:span.author (:author post) ": "] (:body post)]
-      [:p.meta (:created post) "//" [:a {:href (str "/post/" (:id post))} "Link"]]]])
+      [:p.meta (render-date (:created post)) " // " [:a {:href (str "/post/" (:id post))} "Link"]]]])
 
 (rum/defc page [title & children]
   [:html
     [:head
       [:meta {:http-equiv "Content-type" :content "text/html; charset=UTF-8"}]
-      [:style styles]]
+      [:style {:dangerouslySetInnerHTML {:__html styles}}]]
       [:body
         [:header
           [:h1 "Стая"]
             [:p#site_subtitle "This is a link."]]
-        children]])
+        children
+        [:footer
+          [:a {:href "https://github.com/xtbmaster"} "Arthur Aliiev"]
+          ". 2018. All rights aren't reserved."
+          [:br]
+          [:a {:href "/feed" :rel "alternate" :type "application/rss+xml"} "RSS"]]
+
+        [:script {:dangerouslySetInnerHTML {:__html
+          "window.onload = function(){
+                                      reloadSubtitle();
+                                      document.getElementById('site_subtitle').onclick = reloadSubtitle;
+
+                                      }
+          function reloadSubtitle(){
+                                     var subtitles = [
+                                                       'Вы уверены что хотите отменить? - Да / Нет / Отмена',
+                                                       'Произошла ошибка',
+                                                       'Привет!'
+                                                       ];
+                                     var subtitle = subtitles[Math.floor(Math.random() * subtitles.length)];
+                                     var div = document.getElementById('site_subtitle');
+                                     div.innerHTML = subtitle;
+                                     }"}}
+
+          ]]])
 
 (rum/defc index [posts]
   (page "Стая"
@@ -117,6 +151,7 @@
   (str "<!DOCTYPE html>\n" (rum/render-static-markup component)))
 
 (cj/defroutes routes
+  (cjr/resources "/i" {:root "public/i" })
   (cj/GET "/" [:as req]
     {:body (render-html (index posts)) })
   (cj/GET "/write" [:as req]
