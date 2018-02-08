b95af0d48bc3efc8f289854f643e56d78e1bbe1a
xtbmaster <arturaliiev@gmail.com>
2018-02-08 23:36:35 +0200

    gen class added

--- Groundwork/web/blog/src/blog/server.clj
+++ Groundwork/web/blog/src/blog/server.clj
@@ -9,7 +9,8 @@
     [ring.util.response])
   (:import
     [org.joda.time DateTime]
-    [org.joda.time.format DateTimeFormat]))
+    [org.joda.time.format DateTimeFormat])
+  (:gen-class))
 
 (def post-ids ["123" "456"])
 
