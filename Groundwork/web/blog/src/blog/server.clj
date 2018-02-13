85c7cc8fe1acc6d72ee523219b05a2f0be7918e8
xtbmaster <arturaliiev@gmail.com>
2018-02-14 00:35:26 +0200

    Editing post functionality added.
    * Groundwork/web/blog/resources/public/script.js:

--- Groundwork/web/blog/src/blog/server.clj
+++ Groundwork/web/blog/src/blog/server.clj
@@ -1,18 +1,19 @@
 (ns blog.server
   (:require
-   [clojure.edn :as edn]
-   [clojure.java.io :as io]
-   [compojure.core :as compojure]
-   [compojure.route]
-   [immutant.web :as web]
-   [rum.core :as rum]
-   [ring.util.response]
-   [ring.middleware.params]
-   [ring.middleware.multipart-params])
+    [clojure.edn :as edn]
+    [clojure.java.io :as io]
+    [compojure.core :as compojure]
+    [compojure.route]
+    [immutant.web :as web]
+    [rum.core :as rum]
+    [ring.util.response]
+    [ring.middleware.params]
+    [ring.middleware.multipart-params]
+    [clojure.string :as str])
   (:import
-   [org.joda.time DateTime]
-   [org.joda.time.format DateTimeFormat]
-   [java.util UUID])
+    [org.joda.time DateTime]
+    [org.joda.time.format DateTimeFormat]
+    [java.util UUID])
   (:gen-class))
 
 (def post-ids ["123" "456"])
@@ -22,45 +23,50 @@
 
 (def date-formatter (DateTimeFormat/forPattern "dd.MM.YYYY"))
 
+(defn zip [coll1 coll2]
+  (map vector coll1 coll2))
+
 (defn render-date [inst]
   (.print date-formatter (DateTime. inst)))
 
 (rum/defc post [post]
   [:.post
-   [:.post_sidebar
-    [:img.avatar {:src (str "/i/" (:author post) ".gif")}]]
-   [:div
-    (for [name (:pictures post)]
-      [:img {:src (str "/post/" (:id post) "/" name)}])
-    [:p [:span.author (:author post) ": "] (:body post)]
-    [:p.meta (render-date (:created post)) " // " [:a {:href (str "/post/" (:id post))} "Link"]]]])
+    [:.post_sidebar
+      [:img.avatar {:src (str "/i/" (:author post) ".gif")}]]
+    [:div
+      (for [name (:pictures post)]
+        [:img {:src (str "/post/" (:id post) "/" name)}])
+      (for [[p idx] (zip (str/split (:body post) #"\n+") (range))]
+        [:p
+          (when (== 0 idx)
+            [:span.author (:author post) ": "])
+          p])
+      [:p.meta (render-date (:created post)) " // " [:a {:href (str "/post/" (:id post))} "Link"]]]])
 
 
 (rum/defc page [opts & children]
   (let [{ :keys [title index?]
           :or {title "Стая"
                 index? false}} opts]
-
-  [:html
-   [:head
-    [:meta {:http-equiv "Content-type" :content "text/html; charset=UTF-8"}]
-    [:title title]
-    [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0"}]
-    [:style {:dangerouslySetInnerHTML {:__html styles}}]]
-   [:body
-    [:header
-      (if index?
-        [:h1 title]
-        [:h1 [:a {:href "/"} title]])
-        [:p#site_subtitle "This is a link."]]
-    children
-    [:footer
-     [:a {:href "https://github.com/xtbmaster"} "Arthur Aliiev"]
-     ". 2018. All rights aren't reserved."
-     [:br]
-     [:a {:href "/feed" :rel "alternate" :type "application/rss+xml"} "RSS"]]
-
-     [:script {:dangerouslySetInnerHTML {:__html script}}]]]))
+    [:html
+      [:head
+        [:meta {:http-equiv "Content-type" :content "text/html; charset=UTF-8"}]
+        [:title title]
+        [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0"}]
+        [:style {:dangerouslySetInnerHTML {:__html styles}}]]
+      [:body
+        [:header
+          (if index?
+            [:h1 title]
+            [:h1 [:a {:href "/"} title]])
+          [:p#site_subtitle "This is a link."]]
+        children
+        [:footer
+          [:a {:href "https://github.com/xtbmaster"} "Arthur Aliiev"]
+          ". 2018. All rights aren't reserved."
+          [:br]
+          [:a {:href "/feed" :rel "alternate" :type "application/rss+xml"} "RSS"]]
+        [:script {:dangerouslySetInnerHTML {:__html script}}]]]))
 
 
 (defn safe-slurp [source]
@@ -73,8 +79,8 @@
 (defn get-post [post-id]
   (let [path (str "posts/" post-id "/post.edn")]
     (some-> (io/file path)
-            (safe-slurp)
-            (edn/read-string))))
+      (safe-slurp)
+      (edn/read-string))))
 
 
 (defn next-post-id []
@@ -86,19 +92,24 @@
                     (bit-shift-left time 32))]
     (str (UUID. new-high low))))
 
-(next-post-id)
 
 (defn save-post! [post pictures]
-  (let [dir (io/file (str "posts/" (:id post)))]
+  (let [dir (io/file (str "posts/" (:id post)))
+         picture-names (for [[picture idx] (zip pictures (range))
+                              :let [in-name (:filename picture)
+                                     [_ ext] (re-matches #".*(\.[^\.]+)" in-name)]]
+                         (str (:id post) "_" (inc idx) ext))]
     (.mkdir dir)
-    (doseq [[is idx] (map vector pictures (range))])
-    (spit (io/file dir "post.edn") (pr-str post))))
+    (doseq [[picture name] (zip pictures picture-names)]
+      (io/copy (:tempfile picture) (io/file dir name))
+      (.delete (:tempfile picture)))
+    (spit (io/file dir "post.edn") (pr-str (assoc post :pictures (vec picture-names))))))
 
 
 (rum/defc index-page [post-ids]
   (page { :index? true}
-        (for [post-id post-ids]
-          (post (get-post post-id)))))
+    (for [post-id post-ids]
+      (post (get-post post-id)))))
 
 
 (rum/defc post-page [post-id]
@@ -113,16 +124,14 @@
       [:form { :action (str "/post/" post-id "/edit")
                :method "post"
                :enctype "multipart/form-data"}
+        [:.edit_post_picture
+          [:input { :type "file" :name "picture"}]]
         [:.edit_post_body
           [:textarea { :value (:body post "")
                        :placeholder "Пиши сюда..."
                        :name "body"}]]
-        [:.edit_post_picture
-          [:input { :type "file" :name "picture"}]]
         [:.edit_post_submit
-          [:input { :type "submit"
-                    :value (if create? "Создать" "Сохранить")}
-            :button.edit_post_submit ]]])))
+          [:button (if create? "Создать" "Сохранить")]]])))
 
 
 (defn render-html [component]
@@ -130,70 +139,82 @@
 
 
 (defn post-ids []
-  (for [name (seq (.list (io/file "posts")))
-        :let [child (io/file "posts" name)]
-        :when (.isDirectory child)]
-    name))
+  (->>
+    (for [name (seq (.list (io/file "posts")))
+           :let [child (io/file "posts" name)]
+           :when (.isDirectory child)]
+      name)
+    (sort)
+    (reverse)))
 
 
 (compojure/defroutes routes
   (compojure.route/resources "/i" {:root "public/i"})
 
   (compojure/GET "/" []
-    {:body (render-html (index-page (post-ids)))})
+    {:body (render-html (index-page (post-ids))) })
 
   (compojure/GET "/post/new" []
     { :status 303
       :headers { "Location" (str "/post/" (next-post-id) "/edit")}})
- 
+  
   (compojure/GET "/post/:id/:img" [id img]
-    ring.util.response/file-response (str "posts/" id "/" img)))
+    (ring.util.response/file-response (str "posts/" id "/" img)))
 
   (compojure/GET "/post/:post-id" [post-id]
-    {:body (render-html (post-page post-id))}
-   
+    {:body (render-html (post-page post-id))})
+  
+  (compojure/GET "/post/:post-id/edit" [post-id]
+    {:body (render-html (edit-post-page post-id))})
 
-    (compojure/GET "/post/:post-id/edit" [post-id]
-      {:body (render-html (edit-post-page post-id))})
+  (ring.middleware.multipart-params/wrap-multipart-params 
+    (compojure/POST "/post/:post-id/edit" [post-id :as req]
+      (let [params (:multipart-params req)
+             body (get params "body")
+             picture (get params "picture")]
+        (save-post! { :id post-id
+                      :body body
+                      :author "Arthur"};; FIXME author
+          [picture]) 
+        { :status 302
+          :headers { "Location" (str "/post/" post-id)}})))
 
-      (ring.middleware.multipart-params/wrap-multipart-params 
-        (compojure/POST "/post/:post-id/edit" [post-id :as req]
-          (let [params (:multipart-params req)
-                 body (get params "body")
-                 picture (get params "picture")]
-            (save-post! { :id post-id
-                          :body body
-                          :author "Arthur"};; FIXME author
-              [(:stream picture)]) 
-              { :status 302
-                :headers { "Location" (str "/post/" post-id)}}))
-          { :store identity})
-
-    (fn [req]
-      {:status 404
-        :body "404 Page Not Found"}))
+  (fn [req]
+    {:status 404
+      :body "404 Page Not Found"}))
 
 (defn with-headers [handler headers]
   (fn [request]
     (some-> (handler request)
-            (update :headers merge headers))))
+      (update :headers merge headers))))
 
+
+(defn print-errors [handler]
+  (fn [req]
+    (try
+      (handler req)
+      (catch Exception e
+        { :status 500
+          :headers {"Content-Type" "text/plain; charset=utf-8"}
+          :body (with-out-str
+                  (clojure.stacktrace/print-stack-trace (clojure.stacktrace/root-cause e)))}))))
 (def app
   (-> routes
     (ring.middleware.params/wrap-params)
-      (with-headers {"Content-Type" "text/html; charset=utf-8"
-                     "Cache-control" "no-cache"
-                     "Expires" "-1"})))
+    (with-headers {"Content-Type" "text/html; charset=utf-8"
+                    "Cache-control" "no-cache"
+                    "Expires" "-1"})
+    (print-errors)))
 
 (defn -main [& args]
   (let [args-map (apply array-map args)
-        port-str (or (get args-map "-p")
-                     (get args-map "--port")
-                     "8080")]
+         port-str (or (get args-map "-p")
+                    (get args-map "--port")
+                    "8080")]
     (println "Starting server on port" port-str)
     (web/run #'app {:port (Integer/parseInt port-str)})))
 
 (comment
   (def server (-main "--port" "8080"))
   (web/stop server))
-  
+
